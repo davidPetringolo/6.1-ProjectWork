116a0cd61a6e6983badb7029a176cd2c90a530ad
Marco Benetti <Marco Benetti>
2016-04-06 12:05:01 +0200

    Eliminazione parte contatti

--- Gruppo a/webApp/pages/contactController.js
+++
@@ -1,5 +0,0 @@
-
-angular.module('routerApp')
-    .controller('contactController', function($scope){
-        $scope.message = 'Ti trovi nella pagina contatti.';
-    });
--- Gruppo a/webApp/scripts/app.js
+++ Gruppo a/webApp/scripts/app.js
@@ -10,20 +10,10 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'pages/homeView.html',
             controller: 'homeController'
         })
-        .state('contact', {
-            url: '/contact',
-            templateUrl: 'pages/contactView.html',
-            controller: 'contactController'
-        })
         .state('details', {
             url: '/details/:id',
             templateUrl: 'pages/detailsView.html',
             controller: 'detailsController'
-        })
-        .state('edit', {
-            url: '/edit/:id',
-            templateUrl: 'pages/editView.html',
-            controller: 'editController'
         })
     ;
 });
