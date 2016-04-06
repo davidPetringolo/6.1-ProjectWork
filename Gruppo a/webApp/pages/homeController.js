35058b11f33d7abae6042d4a967708e4b8df41e9
davidPetringolo <david.petringolo@gmail.com>
2016-04-06 12:34:56 +0200

    tentativo di adattare lo scheletro

--- Gruppo a/webApp/pages/homeController.js
+++ Gruppo a/webApp/pages/homeController.js
@@ -1,7 +1,19 @@
 
 angular.module('routerApp')
-    .controller('homeController', function($scope, $state){
+    .controller('homeController', function($scope, $state, $http){
         $scope.message = 'Ti trovi in home page';
+
+        $http({
+            method: 'POST',
+            url: 'http://incaneva.it/wp-admin/admin-ajax.php',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: "id:" + $scope.eventID
+            }).then(function(resp){
+                $scope.event = resp.data;
+            }, function (resp){
+                alert("ERROR!");
+            });
+        })
 
         $scope.goToContact = function(){
             $state.go('contact');
