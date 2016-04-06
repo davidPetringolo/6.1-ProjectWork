2ae7e2bc2576a7c66657f07a9b0b714f9faba385
Marco Benetti <Marco Benetti>
2016-04-06 12:34:44 +0200

    Home and script

--- Gruppo a/webApp/pages/homeController.js
+++ Gruppo a/webApp/pages/homeController.js
@@ -1,16 +1,18 @@
 
 angular.module('routerApp')
-    .controller('homeController', function($scope, $state){
-        $scope.message = 'Ti trovi in home page';
-
-        $scope.goToContact = function(){
-            $state.go('contact');
-        };
-
-        $scope.goToFirst = function(){
-            $state.go('details', {id:1});
-        };
-
-        $scope.list = data().getAll();
+    .controller('homeController', function($scope, $state, $http){
+        $http({
+            method: 'POST',
+            url: 'http://incaneva.it/wp-admin/admin-ajax.php',
+            data: {action: 'incaneva_events',
+                blog: '1,6,7,8',
+                old: false,
+                limit: 1
+            }
+        }).then(function successCallback(response) {
+            console.log(response);
+        }, function errorCallback(response) {
+            console.log("ERRORE");
+        });
 
     });
